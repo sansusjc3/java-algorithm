df65fe40188d8daacb437f780d578a79a99e97f9
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2023-06-05 00:46:56 +0900

    [Silver V] Title: 수들의 합 5, Time: 248 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 1;
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+
+        while(end != N) {
+            if (sum == N) {
+                count++; end++; sum += end;
+            } else if (sum>N) {
+                sum -= start;
+                start++;
+            } else {
+                end++; sum+=end;
+            }
+        }
+        System.out.println(count);
+    }
+}
