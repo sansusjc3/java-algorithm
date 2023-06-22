113492d6f7c45c95c21b6ebddc475bf88094f518
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2023-06-22 23:28:45 +0900

    [Gold III] Title: 줄 세우기, Time: 900 ms, Memory: 49340 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        for(int i=0; i<=N; i++) {
+            A.add(new ArrayList<>());
+        }
+        int indegree[] = new int[N+1];
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            A.get(S).add(E);
+            indegree[E]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=N; i++) {
+            if(indegree[i]==0) {
+                queue.offer(i);
+            }
+        }
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+            System.out.println(now + " ");
+            for(int next : A.get(now)) {
+                indegree[next]--;
+                if(indegree[next] ==0) {
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
